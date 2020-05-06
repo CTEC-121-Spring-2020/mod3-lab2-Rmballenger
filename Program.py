97d5c4c45c75adbd2fe79ac60f81262d04026777
Rmballenger <r.ballenger@students.clark.edu>
2020-05-06 12:13:22 -0700

    Quick changes

--- Program.py
+++ Program.py
@@ -3,12 +3,12 @@
 from MyLibrary import *
 
 def main():
-    print("Calling functions in libray")
+    print("Calling functions in library")
     print()
     print("__name__", __name__)
     print()
     function1()
     print()
-    function2()
+    function2(5)
 
 main()
