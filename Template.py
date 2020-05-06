8d3d64f834d9ac80e99aa5468c1a571d299cd861
Rmballenger <r.ballenger@students.clark.edu>
2020-05-06 11:00:47 -0700

    Saving for a reset.

--- Template.py
+++ Template.py
@@ -2,16 +2,22 @@
 CTEC 121
 Robert Ballenger
 Module 3 Lab 2
-<assignment/lab description>
+In Class Lab
 """
 
-""" IPO template
+""" 
+IPO template
 Input(s): list/description
 Process: description of what function does
 Output: return value and description
 """
 
 def main():
-    # code here
+    try:
+        print(4/0)
+    except:
+        print("\nThere was a divide by zero error. Exiting\n")
+        return
+    
 
 main()    
